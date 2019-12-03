77da5d441bcb95598800761fb82240eabab3e409
Amnon Khen <amnon.is@gmail.com>
2019-12-03 12:23:03 +0200

    gateway svc

---
+++ gateway-service/gateway/src/main/java/com/akh/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.akh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
